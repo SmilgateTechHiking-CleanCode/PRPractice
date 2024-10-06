563a8fcc9e818bae6c5d60f3ac9c518531a47e86
star-moon-cloud-k <kspecial44@gmail.com>
2024-10-07 00:10:59 +0900

    FEAT : find all

--- product/product.controller.ts
+++ product/product.controller.ts
@@ -3,19 +3,7 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get('')
-  async getProductListAndTotalPage(@Query() payload: GetProductListReq) {
-    const whereOption: SQL = this.productService.getwhereOptions(payload);
-    const getTotalPagePromise = this.productService.getTotalPage(
-      payload,
-      whereOption,
-    );
-    const findAllPromise = this.productService.getProductList(payload, whereOption);
-
-    const [result, totalPage] = await Promise.all([
-      findAllPromise,
-      getTotalPagePromise,
-    ]);
-
-    return { result, totalPage };
+  findAll(@Query() payload: GetProductListReq) {
+    return this.productService.findAll(payload);
   }
 }
